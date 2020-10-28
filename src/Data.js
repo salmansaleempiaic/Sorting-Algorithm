52b2e76071e72370b71df1c187beb4a136f5db7d
salman <salmansaleempiaic@gmail.com>
2020-10-28 14:41:23 +0500

    JSON Added

---
+++ src/Data.js
@@ -0,0 +1,27 @@
+export const ARTICLES = [
+    {
+      title: "Article 11",
+      upvotes: 1,
+      date: "2020-01-01",
+    },
+    {
+      title: "Article 22",
+      upvotes: 1000,
+      date: "2020-01-02",
+    },
+    {
+      title: "Article 33",
+      upvotes: 2,
+      date: "2019-01-03",
+    },
+    {
+      title: "Article 44",
+      upvotes: 2000,
+      date: "2019-01-04",
+    },
+    {
+      title: "Article 55",
+      upvotes: 5,
+      date: "2018-01-05",
+    },
+  ];
